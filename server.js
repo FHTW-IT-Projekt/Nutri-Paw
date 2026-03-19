345c165faef1b118796d4a687176b56258a8567c
Nyanicoco <anikooborsi@gmail.com>
2026-03-19 17:09:00 +0100

    fixed missin /

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const apiPrefix = 'api/';
+const apiPrefix = '/api/';
 
 app.get(apiPrefix + 'getSampleData', (req, res) => {
   const respone = {
