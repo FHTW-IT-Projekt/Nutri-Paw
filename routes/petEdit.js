0cfff991a1c375e40e79092ee225d0d8a51f3d41
Belli Bulli <sigbel2025@outlook.com>
2026-05-12 23:05:45 +0200

    created petEdit.js in Backend and reorderd server.js

--- routes/petEdit.js
+++ routes/petEdit.js
@@ -3,11 +3,35 @@ import pool from '../db/db.js';
 
 const router = express.Router();
 
-// Route: /api/petedit/:id
+//hier das Tier geadded wenn es noch nicht existiert
+router.post('/', async (req, res) => {
+ // Daten vom Frontend entpacken
+  const { name, species, breed, age, color, diagnosis, behaviour } = req.body;
+
+  try {
+    // Die pet_id wird von der Datenbank automatisch vergeben (Auto-Increment)
+    const [result] = await pool.query(
+      `INSERT INTO pets (name, species, race, age, colour, diagnosis, behaviour) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [name, species, breed, age, color, diagnosis, behaviour]
+    );
+
+    // result.insertId gib die ID des Tieres zurück
+    res.status(201).json({ 
+      message: 'Tier erfolgreich angelegt!',
+      newPetId: result.insertId 
+    });
+  } catch (error) {
+    console.error("SQL Fehler beim Erstellen im Backend:", error);
+    res.status(500).json({ message: 'Serverfehler beim Anlegen des Tieres' });
+  }
+});
+
+
+//hier passiert das update und die Routeist /api/petedit/:id
 router.put('/:id', async (req, res) => {
   const petId = req.params.id;
   
-  // Daten vom Frontend entpacken
+ 
   const { name, species, breed, age, color, diagnosis, behaviour } = req.body;
 
   try {
